cf136124b9955f5cc6a98fcfbe4306f3a4b23d8b
Steve132 <steve@soapforge.com>
2022-07-05 14:11:51 -0400

    Coord type

--- include/imtag.hpp
+++ include/imtag.hpp
@@ -6,18 +6,17 @@
 
 namespace imtag{
 
-template<class label_t>
+template<class label_t,class coord_t=uint16_t>
 class Segment
 {
 public:
-    using coord_t=uint32_t;
-
     coord_t row;
 	coord_t column_start;
 	coord_t column_end;  //one past (exclusive)
 	label_t label;
 	Segment() : row(0),column_start(0),column_end(0),label(0) {}
-	Segment(const coord_t column_start_, const coord_t column_end_, const label_t label_) : column_start(column_start_),column_end(column_end_),label(label_) {}
+	Segment(const coord_t row_,const coord_t column_start_, const coord_t column_end_, const label_t label_): 
+        row(row_),column_start(column_start_),column_end(column_end_),label(label_) {}
 
 	bool overlap(const Segment& b) const
 	{
