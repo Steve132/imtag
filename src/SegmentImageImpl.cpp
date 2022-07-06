20696d4e825c582918a02532b187714b5a9ea268
Steve132 <steve@soapforge.com>
2022-07-06 03:21:06 -0400

    Update dispatches

--- src/SegmentImageImpl.cpp
+++ src/SegmentImageImpl.cpp
@@ -6,10 +6,46 @@ namespace imtag{
 
 template<class label_t>
 void SegmentImageImpl<label_t>::update(const uint8_t* binary_image,ConnectivitySelection cs){
-
+    switch(cs){
+        case ConnectivitySelection::HORIZONTAL:
+            update_compiletime_dispatch(binary_image,cs_tag<ConnectivitySelection::HORIZONTAL>{});
+            break;
+        case ConnectivitySelection::CROSS:
+            update_compiletime_dispatch(binary_image,cs_tag<ConnectivitySelection::CROSS>{});
+            break;
+        case ConnectivitySelection::VERTICAL:
+            update_compiletime_dispatch(binary_image,cs_tag<ConnectivitySelection::VERTICAL>{});
+            break;
+        case ConnectivitySelection::EIGHT_WAY:
+            update_compiletime_dispatch(binary_image,cs_tag<ConnectivitySelection::EIGHT_WAY>{});
+            break;
+    }
 }
 
-
+template<class label_t>
+void SegmentImageImpl<label_t>::update_compiletime_dispatch
+(const uint8_t* binary_image,cs_tag<ConnectivitySelection::HORIZONTAL>)
+{
+    
+}
+template<class label_t>
+void SegmentImageImpl<label_t>::update_compiletime_dispatch
+(const uint8_t* binary_image,cs_tag<ConnectivitySelection::CROSS>)
+{
+    
+}
+template<class label_t>
+void SegmentImageImpl<label_t>::update_compiletime_dispatch
+(const uint8_t* binary_image,cs_tag<ConnectivitySelection::EIGHT_WAY>)
+{
+    
+}
+template<class label_t>
+void SegmentImageImpl<label_t>::update_compiletime_dispatch
+(const uint8_t* binary_image,cs_tag<ConnectivitySelection::VERTICAL>)
+{
+    
+}
 
 template class SegmentImageImpl<uint8_t>;
 template class SegmentImageImpl<uint16_t>;
--- src/SegmentImageImpl.hpp
+++ src/SegmentImageImpl.hpp
@@ -20,11 +20,20 @@ public:
         std::vector<seg_t>
     > segments_by_label;
 
-
     SegmentImageImpl()=default;
     SegmentImageImpl(size_t rows_,size_t cols_){}
 
     void update(const uint8_t* binary_image,ConnectivitySelection cs);
+    void rows_to_labels();
+
+    template<ConnectivitySelection cs>
+    struct cs_tag{};
+
+    void update_compiletime_dispatch(const uint8_t* binary_image,cs_tag<ConnectivitySelection::CROSS>);
+    void update_compiletime_dispatch(const uint8_t* binary_image,cs_tag<ConnectivitySelection::HORIZONTAL>);
+    void update_compiletime_dispatch(const uint8_t* binary_image,cs_tag<ConnectivitySelection::VERTICAL>);
+    void update_compiletime_dispatch(const uint8_t* binary_image,cs_tag<ConnectivitySelection::EIGHT_WAY>);
+    
 };
 
 }
