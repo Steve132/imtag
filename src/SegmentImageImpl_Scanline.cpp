61e703e8977b2d993d5287fdcd4bad7efbe632cb
Steve132 <steve@soapforge.com>
2022-07-06 06:42:09 -0400

    Initial HSCan implementation

--- src/SegmentImageImpl_Scanline.cpp
+++ src/SegmentImageImpl_Scanline.cpp
@@ -1,5 +1,79 @@
 #include "SegmentImageImpl.hpp"
 
+#include<cstdint>
+#include<cstdlib>
+
+template<size_t W>
+struct simd_ops
+{
+    static bool is_aligned(const uint8_t* bimg){
+        return (reinterpret_cast<uintptr_t>(bimg) & (W-1))==0;
+    }
+    template<bool mask>
+    static bool is_all(const uint8_t* bimg);
+    
+    template<bool mask>
+    static size_t skip_align(
+        const uint8_t* bimg,
+        size_t i,
+        const size_t C)
+    {
+        if(is_aligned(bimg+i))
+        {
+            for(;i<(C-W) && is_all<mask>(bimg+i);i+=W){}
+            return i;
+        }
+        else if constexpr(W > 8){
+            return simd_ops<W/2>::template skip_align<mask>(bimg,i,C);
+        }
+        else{
+            return i;
+        }
+    }
+};
+
+template<>
+template<bool mask>
+inline bool simd_ops<8>::is_all(const uint8_t* bimg){
+    uint64_t r=*reinterpret_cast<const uint64_t*>(bimg);
+    if constexpr(mask){
+        return r==0xFFFFFFFFFFFFFFFFULL;
+    }
+    else{
+        return r==0;
+    }
+}
+template<size_t W>
+template<bool mask>
+inline bool simd_ops<W>::is_all(const uint8_t* bimg){
+    return 
+    simd_ops<W/2>::template is_all<mask>(bimg) && 
+    simd_ops<W/2>::template is_all<mask>(bimg+W/2);
+}
+
+#ifdef __AVX2__
+#include<immintrin.h>
+
+template<>
+template<bool mask>
+inline bool simd_ops<32>::is_all(const uint8_t* bimg)
+{
+    __m256i r=*reinterpret_cast<const __m256i*>(bimg);
+    if constexpr(mask)
+    {
+        __m256i o=_mm256_set1_epi16(-1);
+        return _mm256_testc_si256 (r,o);
+    }
+    else
+    {
+        __m256i z= _mm256_setzero_si256();
+        return _mm256_testc_si256 (z,r);
+    }
+}
+
+#endif
+
+
 template<class impl_tag>
 struct compress_scanline;
 
@@ -14,6 +88,7 @@ static void impl(const uint8_t* bimg,size_t rindex,size_t C,MakeSeg&& msfunc){
     while(i<C){
         size_t b=-1;
         for(;i<C;i++){
+            i=simd_ops<128>::skip_align<false>(bimg,i,C);
             if(bimg[i] == 0xFF) {
                 b=i;
                 break;
@@ -22,6 +97,7 @@ static void impl(const uint8_t* bimg,size_t rindex,size_t C,MakeSeg&& msfunc){
         if(i==C) break;
         size_t e=C;
         for(;i<C;i++){
+            i=simd_ops<128>::skip_align<true>(bimg,i,C);
             if(bimg[i] != 0xFF) {
                 e=i;
                 break;
@@ -30,7 +106,6 @@ static void impl(const uint8_t* bimg,size_t rindex,size_t C,MakeSeg&& msfunc){
         msfunc(rindex,b,e);
     }
 }
-
 };
 
 using default_tag=native_tag;
