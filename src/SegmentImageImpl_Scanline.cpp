d60556deacd5e4f210b6cb87f9c204e205e65499
heretodev <heretodev@github.com>
2022-07-06 22:32:40 -0400

    Scanline update.

--- src/SegmentImageImpl_Scanline.cpp
+++ src/SegmentImageImpl_Scanline.cpp
@@ -4,32 +4,32 @@
 #include<cstdlib>
 #include<limits>
 
-#include <omp.h>
-
 template<size_t W>
 struct simd_ops
 {
     static bool is_aligned(const uint8_t* bimg){
         return (reinterpret_cast<uintptr_t>(bimg) & (W-1))==0;
     }
+
+	// check for is all ones or is all zeros for size W
     template<bool mask>
     static bool is_all(const uint8_t* bimg);
     
     template<bool mask>
-    static size_t skip_align(
+	static uint_fast16_t skip(
         const uint8_t* bimg,
-        size_t i,
-        const size_t C)
+		uint_fast16_t i,
+		const uint_fast16_t C)
     {
-        if(is_aligned(bimg+i))
+		if(is_aligned(bimg+i)) // TODO: alignment not required?
         {
             for(;i<(C-W) && is_all<mask>(bimg+i);i+=W){}
             return i;
         }
         else if constexpr(W > 8){
-            return simd_ops<W/2>::template skip_align<mask>(bimg,i,C);
+			return simd_ops<W/2>::template skip<mask>(bimg,i,C);
         }
-        else{
+		else{
             return i;
         }
     }
@@ -54,9 +54,9 @@ inline bool simd_ops<W>::is_all(const uint8_t* bimg){
     simd_ops<W/2>::template is_all<mask>(bimg+W/2);
 }
 
+// Check if all zeros or all 1s for next 32 bytes:
 #ifdef __AVX2__
 #include<immintrin.h>
-
 template<>
 template<bool mask>
 inline bool simd_ops<32>::is_all(const uint8_t* bimg)
@@ -65,17 +65,35 @@ inline bool simd_ops<32>::is_all(const uint8_t* bimg)
     if constexpr(mask)
     {
         __m256i o=_mm256_set1_epi16(-1);
-        return _mm256_testc_si256 (r,o);
+		return _mm256_testc_si256(r,o);
     }
     else
     {
         __m256i z= _mm256_setzero_si256();
-        return _mm256_testc_si256 (z,r);
+		return _mm256_testc_si256(z,r);
     }
 }
 
+template<>
+template<bool mask>
+inline bool simd_ops<64>::is_all(const uint8_t* bimg)
+{
+	__m256i r1=*reinterpret_cast<const __m256i*>(bimg);
+	__m256i r2=*reinterpret_cast<const __m256i*>(bimg+32);
+	if constexpr(mask)
+	{
+		__m256i o=_mm256_set1_epi16(-1);
+		__m256i r=_mm256_and_si256(r1,r2);
+		return _mm256_testc_si256(r,o);
+	}
+	else
+	{
+		__m256i z= _mm256_setzero_si256();
+		__m256i r=_mm256_or_si256(r1,r2);
+		return _mm256_testc_si256(z,r);
+	}
+}
 #endif
-
 
 template<class impl_tag>
 struct compress_scanline;
@@ -87,29 +105,32 @@ struct compress_scanline<native_tag>{
 
 // NOTE: This is BY FAR the performance bottleneck:
 template<class MakeSeg>
-static void impl(const uint8_t* bimg,size_t rindex,size_t C,MakeSeg&& msfunc){
-    size_t i=0;
+static void impl(const uint8_t* bimg,const uint_fast16_t rindex,const uint_fast16_t C,MakeSeg&& msfunc){
+	uint_fast16_t i=0;
 	// Iterate over one row
     while(i<C){
-        size_t b=-1;
+		// Search for 1s
+		uint_fast16_t beginning;
         for(;i<C;i++){
-			i=simd_ops<128>::skip_align<false>(bimg,i,C);
+			i=simd_ops<128>::skip<false>(bimg,i,C);
             if(bimg[i] == 0xFF) {
-                b=i;
+				// Found a 1, break
+				beginning=i;
                 break;
             }
         }
         if(i==C) break;
-        size_t e=C;
+		uint_fast16_t ending=C;
         for(;i<C;i++){
-			i=simd_ops<128>::skip_align<true>(bimg,i,C);
+			i=simd_ops<128>::skip<true>(bimg,i,C);
             if(bimg[i] != 0xFF) {
-                e=i;
+				// Found a non-1, break
+				ending=i;
                 break;
             }
         }
 		// Make the segment:
-        msfunc(rindex,b,e);
+		msfunc(rindex,beginning,ending);
     }
 }
 };
@@ -124,17 +145,19 @@ void SegmentImageImpl<label_t>::compress_scanlines(
     const uint8_t* binary_image,
     size_t R,size_t C,
     std::vector<std::vector<seg_t>>& output_rows){
-    output_rows.resize(R);
+	output_rows.resize(R);
 
+	uint_fast16_t R16 = static_cast<uint_fast16_t>(R);
+	uint_fast16_t C16 = static_cast<uint_fast16_t>(C);
 	#pragma omp parallel for
-	for(size_t r=0;r<R;r++){
+	for(uint_fast16_t r=0;r<R16;r++){
 		// Append segments to this scanline
 		label_t rlabel = 0;
 		auto& rows=output_rows[r];
 		rows.clear();
-		compress_scanline<default_tag>::impl(binary_image+C*r,r,C,
+		compress_scanline<default_tag>::impl(binary_image+C16*r,r,C16,
 			// Make segment (seg_t) function:
-			[&rows,&rlabel](size_t rind,size_t cbegin,size_t cend){
+			[&rows,&rlabel](uint_fast16_t rind,uint_fast16_t cbegin,uint_fast16_t cend){
 				rows.emplace_back(rind,cbegin,cend,rlabel++);
 			}
 		);
