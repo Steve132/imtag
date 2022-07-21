b696e6221dc5cb6ee7ac5e7ca6482c4365ef02f1
heretodev <heretodev@github.com>
2022-07-21 16:15:38 -0400

    Neon intrinsics base for scanline.

---
+++ src/neon_scanline.hpp
@@ -0,0 +1,18 @@
+#ifndef NEON_SCANLINE_HPP
+#define NEON_SCANLINE_HPP
+
+#include<cstdint>
+#include<cstdlib>
+#include "scanline_base.hpp"
+
+#include <arm_neon.h>
+
+namespace neon
+{
+
+
+// uint8_t	vget_lane_u8	(uint8x8_t v, const int lane)
+
+}
+
+#endif // NEON_SCANLINE_HPP
