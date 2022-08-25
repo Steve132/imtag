9e37a000d7a43217a72e703a16f18513edba13aa
Steven B <steve@soapforge.com>
2022-08-25 05:25:13 -0400

    needs arm8

--- src/scanlines/index.hpp
+++ src/scanlines/index.hpp
@@ -1,11 +1,11 @@
 #ifndef SCANLINES_INDEX_HPP
 #define SCANLINES_INDEX_HPP
 
-#if defined(__ARM_NEON)
+#if defined(__ARM_NEON) && defined(__aarch64__)
 #include "neon_scanline.hpp"
 #elif defined(__AVX2__)
 #include "avx2_scanline.hpp"
-#elif defined(__SSE3__)
+#elif defined(__SSE3__) && defined( __SSE4_1__ )
 #include "sse4_scanline.hpp"
 #elif defined(_OPENMP)
 #include "omp_scanline.hpp"
